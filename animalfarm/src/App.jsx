274b43c40b62b323415b09e51904a0468a185515
angeloalanaza08 <angelo.lanaza08@gmail.com>
2023-07-06 03:03:10 +0800

    first commit

---
+++ animalfarm/src/App.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import "./app.css";
+
+function App() {
+  const { search, animals } = useAnimalSearch();
+
+  return (
+    <main>
+      <h1>Animal Farm</h1>
+
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => search(e.target.value)}
+      />
+
+      <ul>
+        {animals.map((animal) => (
+          <Animal key={animal.id} {...animal} />
+        ))}
+
+        {animals.length === 0 && "No animals found"}
+      </ul>
+    </main>
+  );
+}
+
+// Dumb UI component
+function Animal({ type, name, age }) {
+  return (
+    <ul>
+      <li>
+        <strong>{type}</strong> {name} ({age} years old)
+      </li>
+    </ul>
+  );
+}
+
+// Custom Hook
+function useAnimalSearch() {
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem("lastQuery");
+    search(lastQuery);
+  }, []);
+
+  const search = async (q) => {
+    const response = await fetch(
+      "http://localhost:8080?" + new URLSearchParams({ q })
+    );
+    const data = await response.json();
+    setAnimals(data);
+
+    localStorage.setItem("lastQuery", q);
+  };
+
+  return { search, animals };
+}
+
+export default App;
